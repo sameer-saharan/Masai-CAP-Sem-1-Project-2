ac8ed794097094edfcdffb0cdbcc703114bf3d79
Sameer <114926999+sameer-saharan@users.noreply.github.com>
2023-07-27 18:51:09 +0530

    Add files via upload

---
+++ Masai CAP Sem-1 Project 2/main_calendar.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkcalendar import Calendar
+from datetime import date
+
+def SelectDate(event):
+    selectedDate = cal.get_date()    # Get selected date
+    dateButton.config(text=selectedDate)    # Update dateButton text
+
+
+root = tk.Tk()  # Main window
+root.title("Calendar")
+
+cal = Calendar(root, selectmode='day', date_pattern='mm-dd-yyyy')  # Calendar widget
+cal.pack(pady=25)
+
+dateButton = tk.Button(root, text="Select Date", command=lambda: cal.selection_set(date.today()))   # Button to select date
+dateButton.pack(pady=15)
+
+cal.bind("<<CalendarSelected>>", SelectDate)
+root.mainloop()
